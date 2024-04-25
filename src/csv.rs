bfc46415b96a39c6387092ce945945b800049b89
Ivan Fraixedes <ivan@fraixed.es>
2024-04-25 13:17:10 +0200

    refactor: Use lazy CSV reader
    Use a Polars Lazy CSV reader because it's optimal.

--- src/csv.rs
+++ src/csv.rs
@@ -4,91 +4,113 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use polars::datatypes::DataType;
-use polars::prelude::{
-    col, AnyValue, CsvReader, DataFrame, Expr, IntoLazy, LiteralValue, QuantileInterpolOptions,
-    Schema, SerReader,
-};
+use polars::lazy::frame::{LazyCsvReader, LazyFileListReader, LazyFrame};
+use polars::prelude::{col, AnyValue, Expr, LiteralValue, QuantileInterpolOptions, Schema};
+
+const COLUMNS: &[&str] = &["timestamp", "elapsedtime_seconds", "trace_id", "jaeger_url"];
 
 pub struct Analyzer {
-    df: DataFrame,
+    df: LazyFrame,
 }
 
 impl Analyzer {
     pub fn from_csv(file: &PathBuf) -> Result<Self, Error> {
         let mut schema = Schema::with_capacity(3);
-        schema.with_column("timestamp".into(), DataType::String);
-        schema.with_column("elapsedtime_seconds".into(), DataType::Float64);
-        schema.with_column("trace_id".into(), DataType::String);
-        schema.with_column("jaeger_url".into(), DataType::String);
+        schema.with_column(COLUMNS[0].into(), DataType::String);
+        schema.with_column(COLUMNS[1].into(), DataType::Float64);
+        schema.with_column(COLUMNS[2].into(), DataType::String);
+        schema.with_column(COLUMNS[3].into(), DataType::String);
 
-        // TODO: change it to use LazyCsvReader
-        let df = CsvReader::from_path(file)
-            .map_err(|e| Error::Polars {
-                context: format!("reading CSV file '{}'", file.to_str().unwrap_or("")),
-                source: e,
-            })?
+        let df = LazyCsvReader::new(file)
             .has_header(true)
             .with_schema(Some(Arc::new(schema)))
-            .finish()
+            .with_cache(true)
+            .finish_no_glob()
             .map_err(|e| Error::Polars {
                 context: format!(
-                    "creating a DataFrame from CSV file {}",
+                    "creating LazyFrame from CSV file {}",
                     file.to_str().unwrap_or("")
                 ),
                 source: e,
             })?;
-
-        if df.is_empty() {
-            return Err(Error::InvalidData {
-                reason: format!(
-                    "there is no data in the CSV file {}",
-                    file.to_str().unwrap_or(""),
-                ),
-            });
-        }
 
         Ok(Self { df })
     }
 
     // TODO: provide all the data of the row.
     pub fn fastest(&self) -> Result<f64, Error> {
-        Ok(self.df
-            .column("elapsedtime_seconds")
-            .map_err(|e| Error::Polars { context: r#"selecting column "elapsedtime_seconds""#.into(), source: e })?
+        self.df
+            .clone()
+            .select(&[col(COLUMNS[1])])
+            .min()
+            .map_err(|e| Error::Polars {
+                context: format!(r#"creating query for calculating the minimum of "{}" column"#, COLUMNS[1]),
+                source: e,
+            })?
+            .collect()
+            .map_err(|e| Error::Polars {
+                context: format!(r#"executing the lazy operations for calculating the minimum value of the "{} column"#, COLUMNS[1]),
+                source: e,
+            })?
+            .column(COLUMNS[1])
+            .map_err(|e| Error::Polars {
+                context: format!(r#"selecting column "{}" from collected data frame""#, COLUMNS[1]),
+                source: e,
+            })?
             .min::<f64>()
-            .map_err(|e| Error::Polars { context:r#"calculating the minimum value of the "elapsedtime_seconds" column"#.into(), source: e })?
-            .expect("BUG: missing a checks on DataFrame creation or modification to ensure that they are not empty")
-        )
+            .map_err(|e| Error::Polars {
+                context: format!(r#"calculating the minimum value of the column "{}" from collected data frame""#, COLUMNS[1]),
+                source: e,
+            })?
+            .ok_or_else(|| Error::InvalidData { reason: "CSV file is empty".into() })
     }
 
     // TODO: provide all the data of the row.
     pub fn slowest(&self) -> Result<f64, Error> {
-        Ok(self.df
-            .column("elapsedtime_seconds")
-            .map_err(|e| Error::Polars { context: r#"selecting column "elapsedtime_seconds""#.into(), source: e })?
+        self.df
+            .clone()
+            .select(&[col(COLUMNS[1])])
+            .max()
+            .map_err(|e| Error::Polars {
+                context: format!(r#"creating query for calculating the maximum of "{}" column"#, COLUMNS[1]),
+                source: e,
+            })?
+            .collect()
+            .map_err(|e| Error::Polars {
+                context: format!(r#"executing the lazy operations for calculating the maximum value of the "{} column"#, COLUMNS[1]),
+                source: e,
+            })?
+            .column(COLUMNS[1])
+            .map_err(|e| Error::Polars {
+                context: format!(r#"selecting column "{}" from collected data frame""#, COLUMNS[1]),
+                source: e,
+            })?
             .max::<f64>()
-            .map_err(|e| Error::Polars { context:r#"calculating the minimum value of the "elapsedtime_seconds" column"#.into(), source: e })?
-            .expect("BUG: missing a checks on DataFrame creation or modification to ensure that they are not empty")
-        )
+            .map_err(|e| Error::Polars {
+                context: format!(r#"calculating the maximum value of the column "{}" from collected data frame""#, COLUMNS[1]),
+                source: e,
+            })?
+            .ok_or_else(|| Error::InvalidData { reason: "CSV file is empty".into() })
     }
 
     pub fn percentile(&self, percentile: f64) -> Result<f64, Error> {
         let res = self
             .df
             .clone()
-            .lazy()
-            .select(&[col("elapsedtime_seconds")])
+            .select(&[col(COLUMNS[1])])
             .quantile(
                 Expr::Literal(LiteralValue::Float64(percentile)),
                 QuantileInterpolOptions::Higher,
             )
             .map_err(|e| Error::Polars {
-                context: format!("applying quantile operation for percentile: {percentile}"),
+                context: format!("creating query for calculating the {percentile} percentile"),
                 source: e,
             })?
             .collect()
             .map_err(|e| Error::Polars {
-                context: format!("executing quantile operation for percentile: {percentile}"),
+                context: format!(
+                    "executing the lazy operations for calculating the {percentile} percentile"
+                ),
                 source: e,
             })?;
 
@@ -96,12 +118,13 @@ impl Analyzer {
             Some(v) => match v[0] {
                 AnyValue::Float64(f) => Ok(f),
                 _ => panic!(
-                    r#"unexpected type returned by quantile operation on "elapsedtime_seconds" column"#
+                    r#"unexpected type returned by quantile operation on "{}" column"#,
+                    COLUMNS[1],
                 ),
             },
-            None => panic!(
-                r#"BUG: missing a checks on DataFrame creation or modification to ensure that they are not empty"#
-            ),
+            None => Err(Error::InvalidData {
+                reason: "CSV file is empty".into(),
+            }),
         }
     }
 }
